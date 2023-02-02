25c75973ddbd7a030fc86fc4446b6eece4e2bfc4
yumi yonemori <yumiyonemori@gmail.com>
2023-02-02 21:29:03 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :name, presence: true, length: { maximaum:30 }
-  validates :email, presence: true, length: { maximaum:255}
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: { maximum:30 }
+  validates :email, presence: true, length: { maximum:255}, uniqueness: true,
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 end
