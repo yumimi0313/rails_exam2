c5474f5de5305ef85217d9691f2635ae853e7e58
yumi yonemori <yumiyonemori@gmail.com>
2023-02-03 14:57:14 +0900

    アソシエーション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def confirm
+    @post = Post.new(post_params)
+    #postsはモデルUserで定義したアソシエーション、アソシエーション状態のインスタンスを作るときはbuild、ログイン中のユーザのpostをbuild(new)する
+    @post = current_user.posts.build(post_params) 
+    #バリデーションが発生するタイミング、値が保存される時、よって手動でバリデーションに失敗した場合は投稿画面に戻す
+    render :new if post.invalid? 
+  end
+
   def edit
   end
 
   def create
     @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -33,10 +40,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -45,8 +50,7 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to posts_path, notice: "Post was successfully destroyed." }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-
 end
  
