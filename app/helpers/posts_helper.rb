249ae93f2e2b808329741343945d8554b6b5d79d
yumi yonemori <yumiyonemori@gmail.com>
2023-02-07 13:06:47 +0900

    エラー解消：確認画面、CRUDアクション時

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,9 +2,9 @@
 module PostsHelper
   def choose_or_edit
     #アクションネームで分岐したい時使う、確認画面で戻るボタンを押、createアクションで戻るボタンを押した時の処理を実装したため、アクションネームがcreateアクションになっている
-    if action_name == 'new' || action_name == 'edit'
+    if action_name == 'new' || action_name == 'create'
         confirm_posts_path
-    else
+    elsif
       action_name == 'edit'
         post_path
     end 
